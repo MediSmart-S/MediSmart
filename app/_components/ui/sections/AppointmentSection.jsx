d575fadb10f7e077d6a2e2b3124fdb00fe593003
TahaShameli <112875671+TahaShameli@users.noreply.github.com>
2025-01-28 19:22:37 +0330

    Update AppointmentSection.jsx

--- app/_components/ui/sections/AppointmentSection.jsx
+++ app/_components/ui/sections/AppointmentSection.jsx
@@ -15,14 +15,16 @@ const AppointmentSection = () => {
 
   return (
     <section ref={sectionRef} className="relative bg-slate-50">
-      <SectionHeader
-        title="رزرو قرار ملاقات"
-        description="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است"
-        withDivider={false}
-      />
+      <div className="relative z-10">
+        <SectionHeader
+          title="رزرو قرار ملاقات"
+          description="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است"
+          withDivider={false}
+        />
+      </div>
 
       <span className="absolute top-0 right-0">
-        <Image src={headImage} alt="عکس موج سفید" />
+        <Image src={headImage} alt="عکس" />
       </span>
       <span className="absolute bottom-0 left-0">
         <Image src={footerImage} alt="عکس موج سفید" />
